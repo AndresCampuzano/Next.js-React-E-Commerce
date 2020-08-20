46aacc3ba31b733ff9527817e74bd98323718069
Andres Campuzano <campuxano88@gmail.com>
2020-08-20 10:33:00 -0500

    Metrics and export added.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,11 @@ import '../global.css'
 
 import CartProvider from '@store/Cart'
 
+// Analytics
+export function reportWebVitals(metric: any) {
+  console.log(metric)
+}
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Aditional props
   // Aditional layout
